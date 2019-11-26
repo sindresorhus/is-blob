4d7e5bbeeb38d372dbfa476c127df191f9ebb29d
Sindre Sorhus <sindresorhus@gmail.com>
2019-11-26 22:10:44 +0700

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,8 +1,8 @@
-import { JSDOM } from 'jsdom';
-import { expectType } from 'tsd';
-import isBlob = require(".");
+import {JSDOM} from 'jsdom';
+import {expectType} from 'tsd';
+import isBlob = require('.');
 
-const { window } = new JSDOM();
+const {window} = new JSDOM();
 
 expectType<boolean>(isBlob(new window.Blob()));
 expectType<boolean>(isBlob(Buffer.alloc(1)));
